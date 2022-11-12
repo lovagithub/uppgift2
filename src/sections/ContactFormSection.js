87d33690dcdfa642c872511a343d037e20ada65c
Lova Juhlin <lova.juhlin@outlook.com>
2022-11-12 20:20:19 +0100

    fungerande contact!

--- src/sections/ContactFormSection.js
+++ src/sections/ContactFormSection.js
@@ -8,17 +8,16 @@ const ContactFormSection = () => {
                 <h2>Come in Contact with Us</h2>
                 <form noValidate>
                 <div>
-                    <imput id="name" type="text" placeholder="Your Name" />
+                    <input id="name" type="text" placeholder="Your Name" />
                     <div className="errorMessage"></div>
                     </div>
                     <div>
-                    <imput id="name" type="email" placeholder="Your Mail" />
+                    <input id="name" type="email" placeholder="Your Mail" />
                     <div className="errorMail"></div>
                     </div>
                     <div className="textarea">
                     <textarea id="comment" placeholder="Comments"></textarea>                   
                     </div>
-                  
                     <div>
                         <button type="submit" className="btn-theme">Post Comments</button> 
                      </div>
--- src/sections/MapSection.js
+++ src/sections/MapSection.js
@@ -6,9 +6,9 @@ const MapSection = () => {
         className="google-map"
         title="google-map"
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d65096.35000246335!2d18.022222!3d59.34529200000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xac5dd727a529fe56!2sEC%20Utbildning!5e0!3m2!1ssv!2sse!4v1668249804474!5m2!1ssv!2sse"
-        allowfullscreen="" 
+        allowFullScreen="" 
         loading="lazy" 
-        referrerpolicy="no-referrer-when-downgrade">      
+        referrerPolicy="no-referrer-when-downgrade">      
         </iframe>
     )
 }
